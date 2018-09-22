4e699311553ad4495fc12ca951c39bd809a9ec68
ac92 <alfredc_1092@hotmail.com>
2018-09-21 17:56:32 -0700

    Added incomplete dialog activity to enter new drug info

--- app/src/main/java/com/example/android/dialysisbuddy/DrugsDialogActivity.java
+++ app/src/main/java/com/example/android/dialysisbuddy/DrugsDialogActivity.java
@@ -12,5 +12,6 @@ public class DrugsDialogActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drugs_dialog);
     }
 }
