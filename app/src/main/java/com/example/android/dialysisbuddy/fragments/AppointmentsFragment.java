31ddc046bef07b9b3c6edbe4ddefca2823ac796d
ac92 <alfredc_1092@hotmail.com>
2018-09-25 21:52:11 -0700

    Added calendar view

--- app/src/main/java/com/example/android/dialysisbuddy/fragments/AppointmentsFragment.java
+++ app/src/main/java/com/example/android/dialysisbuddy/fragments/AppointmentsFragment.java
@@ -1,10 +1,23 @@
 package com.example.android.dialysisbuddy.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.dialysisbuddy.R;
 
 /**
  * Created by alfredchang on 2018-01-22.
  */
 
 public class AppointmentsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        super.onCreateView(inflater, parent, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_appointments_main, parent, false);
+        return rootView;
+    }
 }
