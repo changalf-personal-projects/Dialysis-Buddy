ce5c3e268f3050d9a065b68c763480a4235b9fba
ac92 <alfredc_1092@hotmail.com>
2018-09-25 23:04:40 -0700

    Set up click listener

--- app/src/main/java/com/example/android/dialysisbuddy/fragments/AppointmentsFragment.java
+++ app/src/main/java/com/example/android/dialysisbuddy/fragments/AppointmentsFragment.java
@@ -1,12 +1,17 @@
 package com.example.android.dialysisbuddy.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import com.example.android.dialysisbuddy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by alfredchang on 2018-01-22.
@@ -14,10 +19,26 @@ import com.example.android.dialysisbuddy.R;
 
 public class AppointmentsFragment extends Fragment {
 
+    @BindView(R.id.calendar) CalendarView mCalendar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         super.onCreateView(inflater, parent, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_appointments_main, parent, false);
+
+        ButterKnife.bind(this, rootView);
+        setCalendarClickListener();
+
         return rootView;
+    }
+
+    private void setCalendarClickListener() {
+        mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+
+            }
+        });
     }
 }
