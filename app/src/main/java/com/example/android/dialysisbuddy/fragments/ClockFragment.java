bec4d5af1d5bf4cb4353529597d0821908d8eeac
ac92 <alfredc_1092@hotmail.com>
2018-09-29 00:19:02 -0700

    Display time when clock is submitted

--- app/src/main/java/com/example/android/dialysisbuddy/fragments/ClockFragment.java
+++ app/src/main/java/com/example/android/dialysisbuddy/fragments/ClockFragment.java
@@ -1,12 +1,18 @@
 package com.example.android.dialysisbuddy.fragments;
 
 import android.app.Dialog;
-import android.support.v4.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
-import android.text.format.DateFormat;
+import com.example.android.dialysisbuddy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by alfredchang on 2018-09-28.
@@ -17,13 +23,18 @@ public class ClockFragment extends DialogFragment implements TimePickerDialog.On
     private final int HOUR = 12;
     private final int MINUTE = 0;
 
+    @BindView(R.id.time) TextView mTime;
+
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+        ButterKnife.bind(this, getActivity());
         return new TimePickerDialog(getActivity(), this, HOUR, MINUTE, DateFormat.is24HourFormat(getActivity()));
     }
 
     @Override
-    public void onTimeSet(TimePicker timePicker, int i, int i1) {
-
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        mTime.setText(hour + ":" + minute);
     }
 }
