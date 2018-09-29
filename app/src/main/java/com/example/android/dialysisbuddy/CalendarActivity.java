e3c0e84baff1cdfc636f07f0f65a3942b5001559
ac92 <alfredc_1092@hotmail.com>
2018-09-28 20:02:00 -0700

    Added edit text boxes

--- app/src/main/java/com/example/android/dialysisbuddy/CalendarActivity.java
+++ app/src/main/java/com/example/android/dialysisbuddy/CalendarActivity.java
@@ -14,7 +14,7 @@ public class CalendarActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appointments_main);
+        setContentView(R.layout.activity_calendar_main);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.container,
--- app/src/main/java/com/example/android/dialysisbuddy/fragments/CalendarFragment.java
+++ app/src/main/java/com/example/android/dialysisbuddy/fragments/CalendarFragment.java
@@ -1,6 +1,14 @@
 package com.example.android.dialysisbuddy.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.dialysisbuddy.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by alfredchang on 2018-09-25.
@@ -8,5 +16,14 @@ import android.support.v4.app.Fragment;
 
 public class CalendarFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_calendar_main, container, false);
+
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
 
 }
