3ecd3794f4faaad441071aff06e08be001007ae5
ac92 <alfredc_1092@hotmail.com>
2018-09-29 23:47:18 -0700

    Added constructor

--- app/src/main/java/com/example/android/dialysisbuddy/adapters/DatesRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/dialysisbuddy/adapters/DatesRecyclerViewAdapter.java
@@ -1,14 +1,27 @@
 package com.example.android.dialysisbuddy.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.android.dialysisbuddy.models.Date;
+
+import java.util.List;
 
 /**
  * Created by alfredchang on 2018-09-29.
  */
 
 public class DatesRecyclerViewAdapter extends RecyclerView.Adapter<DatesRecyclerViewAdapter.DatesViewHolder> {
+
+    private List<Date> mListOfDates;
+    private Context mContext;
+
+    public DatesRecyclerViewAdapter(List<Date> listOfDates, Context context) {
+        mListOfDates = listOfDates;
+        mContext = context;
+    }
 
     @Override
     public DatesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
