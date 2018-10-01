3aef055a6a0eddee92351d06c394595916b11e58
ac92 <alfredc_1092@hotmail.com>
2018-10-01 00:37:09 -0700

    Added new model classes

---
+++ app/src/main/java/com/example/android/dialysisbuddy/models/Task.java
@@ -0,0 +1,18 @@
+package com.example.android.dialysisbuddy.models;
+
+/**
+ * Created by alfredchang on 2018-10-01.
+ */
+
+public class Task {
+
+    private String mTask;
+
+    public Task(String task) {
+        mTask = task;
+    }
+
+    public String getTask() {
+        return mTask;
+    }
+}
---
+++ app/src/main/java/com/example/android/dialysisbuddy/models/Time.java
@@ -0,0 +1,18 @@
+package com.example.android.dialysisbuddy.models;
+
+/**
+ * Created by alfredchang on 2018-10-01.
+ */
+
+public class Time {
+
+    private String mTime;
+
+    public Time(String time) {
+        mTime = time;
+    }
+
+    public String getTime() {
+        return mTime;
+    }
+}
