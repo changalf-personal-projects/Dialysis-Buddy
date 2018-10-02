6b7d9e1088d0cb0cfc9df48957764feb3686e736
ac92 <alfredc_1092@hotmail.com>
2018-10-02 01:19:49 -0700

    Created method to test for correctness of appointment objects

--- app/src/main/java/com/example/android/dialysisbuddy/models/Appointment.java
+++ app/src/main/java/com/example/android/dialysisbuddy/models/Appointment.java
@@ -1,10 +1,14 @@
 package com.example.android.dialysisbuddy.models;
 
+import android.util.Log;
+
 /**
  * Created by alfredchang on 2018-10-01.
  */
 
 public class Appointment {
+
+    private final String LOG_TAG = Appointment.class.getSimpleName();
 
     private String mTask;
     private String mTime;
@@ -26,5 +30,10 @@ public class Appointment {
 
     public String getDate() {
         return mDate;
+    }
+
+    // Method to test appointment object is right.
+    private void checkForCorrectAppointment() {
+        Log.v(LOG_TAG, "Debugging appointment: " + mTask + ": " + mTime + ": " + mDate);
     }
 }
